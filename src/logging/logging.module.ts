54541810e16fad4b41e6a976b0b985badf60c69a
Martin Hylleberg <mhylle@yahoo.com>
2025-11-24 22:01:03 +0100

    feat(logging): update logging module with LogService and entity

--- src/logging/logging.module.ts
+++ src/logging/logging.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 import { ResearchLogger } from './research-logger.service';
+import { LogService } from './log.service';
+import { LogEntryEntity } from './entities/log-entry.entity';
 
 @Module({
-  providers: [ResearchLogger],
-  exports: [ResearchLogger],
+  imports: [
+    TypeOrmModule.forFeature([LogEntryEntity]),
+    EventEmitterModule.forRoot(),
+  ],
+  providers: [ResearchLogger, LogService],
+  exports: [ResearchLogger, LogService, TypeOrmModule],
 })
 export class LoggingModule {}
